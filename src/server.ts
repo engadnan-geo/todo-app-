dfd9f18ab97910c2f43f871eb1a02feefe05660a
engadnan-geo <adnangeo266@gmail.com>
2025-11-04 07:46:26 -0800

    first commite

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("🚀 TypeScript + ts-node-dev is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => 
+  console.log(`Server is running on http://localhost:${PORT}`));
